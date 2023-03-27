b0006a35c9425d6014071cf5afc41d27e7657db7
mateog147 <mateo.gutierrez@sofka.com.co>
2023-03-27 09:16:18 -0500

    fix: show info message only the firts loafd

--- src/app/components/choose-form/choose-form.component.ts
+++ src/app/components/choose-form/choose-form.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import swal from'sweetalert2';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-choose-form',
   templateUrl: './choose-form.component.html',
-  styleUrls: ['./choose-form.component.css']
+  styleUrls: ['./choose-form.component.css'],
 })
-export class ChooseFormComponent implements OnInit {
+export class ChooseFormComponent implements AfterViewInit{
+  constructor(private router: Router) {}
 
-  constructor(private router:Router) { }
-
-  ngOnInit(): void {
-    this.showInstructions();
+  ngAfterViewInit(): void {
+    if (!sessionStorage.getItem('siteInit')) {
+      this.showInstructions();
+      sessionStorage.setItem('siteInit', 'true');
+    }
   }
 
-  goToForm(link:string){
+  goToForm(link: string) {
     this.router.navigate([link]);
   }
 
-  showInstructions(){
+  showInstructions() {
     swal.fire(
       '',
       'Esta herramienta solo te da un primer acercamiento para comparar si comprar o arrendar, para tomar tu decisión debes estudiar otros factores',
       'success'
-    )
+    );
   }
-
 }
